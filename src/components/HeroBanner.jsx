ea550b3383b204526020a5fb2147217c802b0f4a
Masum Billah <emasumbillah@gmail.com>
2025-08-25 15:49:07 +0600

    image issue solved

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react';
 import '../styles/HeroBanner.css';
 
 // Import images so bundler can resolve paths correctly
-import banner1 from '../assets/banner1.jpg';
+import banner1 from '../assets/banner1.jpeg';
 import banner2 from '../assets/banner2.jpg';
 import banner3 from '../assets/banner3.jpg';
 import banner4 from '../assets/banner4.jpg';
